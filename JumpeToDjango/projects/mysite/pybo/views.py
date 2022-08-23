fe9e01a8851e7381051f0e6618dea3d23d4c5181
jih3508 <whdlrgus32@naver.com>
2022-08-24 02:12:48 +0900

    장고 1.static 2. 부트스트랩 3. 템플릿 상속 4. 폼

--- JumpeToDjango/projects/mysite/pybo/views.py
+++ JumpeToDjango/projects/mysite/pybo/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Question, Answer
+from django.http import HttpResponseNotAllowed
+from .form import QuestionForm, AnswerForm
 
 # Create your views here.
 
@@ -16,7 +18,27 @@ def detail(request, question_id):
 
 def answer_create(request, question_id):
     question = get_object_or_404(Question, pk= question_id)
-    #question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
-    answer = Answer(question=question, content=request.POST.get('content'), create_date=timezone.now())
-    answer.save()
-    return redirect('pybo:detail', question_id=question.id)
+    if request.method == "POST":
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect("pybo:detail", question_id = question.id)
+    else:
+        return HttpResponseNotAllowed('Only POST is possible.')
+    context = {'question' : question, 'form' : form}
+    return render(request, 'pybo/question_detail.html', context)
+
+def question_create(requset):
+    if requset.method == "POST":
+        form = QuestionForm(requset.POST)
+        if form.is_valid():
+            question = form.save(commit=False) # 임시 저장하여 question 객체르 리턴받는다.
+            question.create_date = timezone.now() # 실제 저장을 위해 작성일시를 설정한다.
+            question.save() # 데이터를 실제로 저장한다.
+            return redirect('pybo:index')
+    else:
+        form = QuestionForm()
+    return render(requset, 'pybo/question_form.html', {'form' : form})
