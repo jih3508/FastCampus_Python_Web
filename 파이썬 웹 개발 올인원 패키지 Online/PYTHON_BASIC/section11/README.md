dbeee8cb56816d83a734976fb943bc903071a5c1
jih3508 <whdlrgus32@naver.com>
2022-09-21 03:08:51 +0900

    11.Excel, CSV 읽기 쓰기

---
+++ 파이썬 웹 개발 올인원 패키지 Online/PYTHON_BASIC/section11/README.md
@@ -0,0 +1,7 @@
+# 11. Excel, CSV 읽기 쓰기
+
+### Excel, CSV 처리
+- CSV 읽기
+- CSV 쓰기
+- XSL, XLSX 읽기
+- 패키지 설치
---
+++ 파이썬 웹 개발 올인원 패키지 Online/PYTHON_BASIC/section11/section11.py
@@ -0,0 +1,78 @@
+# Section11
+# 파이썬 외부 파일 처리
+# 파이썬 Excel, CSV 파일 읽기 및 쓰기
+
+# CSV : MIME - text/csv
+import csv
+
+# 예제1
+with open('./section11/resource/sample1.csv', 'r') as f:
+    reader = csv.reader(f)
+    # next(raader) Header 스킵
+
+    # 확인
+    print(reader)
+    print(type(reader))
+    print(dir(reader))
+    print()
+
+    for c in reader:
+        print(c)
+
+# 예제2
+with open('./section11/resource/sample2.csv', 'r') as f:
+    reader = csv.reader(f, delimiter='|')
+    # next(raader) Header 스킵
+
+    # 확인
+    print(reader)
+    print(type(reader))
+    print(dir(reader))
+    print()
+
+    for c in reader:
+        print(c)
+
+# 예제3 (Dict변환)
+with open('./section11/resource/sample1.csv', 'r') as f:
+    reader = csv.DictReader(f)
+
+    for c in reader:
+        for k, v in c.items():
+            print(k, v)
+        print('-------------')
+
+# 예제4
+w = [[1,2,3],[4, 5, 6], [7, 8, 9], [10,11,12],[13, 14, 15], [16, 17, 18]]
+
+with open('./section11/resource/sample3.csv', 'w', newline='') as f:
+    wt = csv.writer(f)
+
+    for v in w:
+        wt.writerow(v)
+
+# XSL, XLSX
+# openpyxl, xlsxwriter, xlrd, xlwt, xlutils
+# pandas 를 주로 사용(openpyxl, xlrd)
+# pip install xlrd
+# pip install openpyxl
+# pip install pandas
+
+import pandas as pd
+
+xlsx = pd.read_excel('./section11/resource/sample.xlsx')
+
+
+# 상위 데이터 확인
+print(xlsx.head())
+print()
+
+# 데이터 확인
+print(xlsx.tail())
+
+# 데이터 확인
+print(xlsx.shape) # 행, 열
+
+# 엑셀 or CSV 다시쓰기
+xlsx.to_excel('./section11/resource/result.xlsx', index=False)
+xlsx.to_csv('./section11/resource/result.csv', index=False)
